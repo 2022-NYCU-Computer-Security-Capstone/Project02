2fac3e67edfb6b22b1f41537b058c7d2ee287307
Yojahuang <yojahuang30613@gmail.com>
2022-04-10 07:08:17 +0800

    Finish part 1 of MITM

--- MITM.py
+++ MITM.py
@@ -1,8 +1,33 @@
-from scapy.all import *
+import scapy.all as scapy
 
 def getGatewayIP():
-    result = conf.route.route("0.0.0.0")[2]
+    result = scapy.conf.route.route("0.0.0.0")[2]
     return result
 
+def scan(ip):
+    arpRequest = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arpRequestBroadcast = broadcast / arpRequest
+
+    answeredList = scapy.srp(arpRequestBroadcast, timeout=3, verbose=False)[0]
+
+    resultList = []
+    for element in answeredList:
+        resultDict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        resultList.append(resultDict)
+    return resultList
+
 if "__main__" == __name__:
-    print("Gateway IP: ", getGatewayIP())
+    gw = getGatewayIP()
+    print("Gateway IP: ", gw)
+
+    devices = scan(gw + "/24")
+
+    print("Avaliable devices")
+    print("---------------------------")
+    print("IP                 MAC     ")
+    print("---------------------------")
+
+
+    for item in devices:
+        print(item["ip"], item["mac"], sep = '\t')
