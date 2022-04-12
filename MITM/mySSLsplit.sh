5aea835fdfef60425b86d72f5a772d89e8c722b7
Yojahuang <yojahuang30613@gmail.com>
2022-04-12 19:24:13 +0800

    Correct the file path of mySSLsplit command.

--- MITM/mySSLsplit.sh
+++ MITM/mySSLsplit.sh
@@ -1,4 +1,4 @@
-mkdir logdir
+mkdir MITM/logdir
 mkdir /tmp/sslsplit
 
 sysctl -w net.ipv4.ip_forward=1
@@ -10,4 +10,4 @@ iptables -t nat -A PREROUTING -p tcp --dport 465 -j REDIRECT --to-ports 8443
 iptables -t nat -A PREROUTING -p tcp --dport 993 -j REDIRECT --to-ports 8443
 iptables -t nat -A PREROUTING -p tcp --dport 5222 -j REDIRECT --to-ports 8080
 
-sslsplit -D -l connections.log -j /tmp/sslsplit/ -S logdir/ -k ca.key -c ca.crt ssl 0.0.0.0 8443 tcp 0.0.0.0 8080
+sslsplit -D -l connections.log -j /tmp/sslsplit/ -S MITM/logdir/ -k ca.key -c ca.crt ssl 0.0.0.0 8443 tcp 0.0.0.0 8080
