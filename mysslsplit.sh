00007b8978d805efb4dc616de5a618c9acd4274e
Yojahuang <yojahuang30613@gmail.com>
2022-04-12 15:53:58 +0800

    Add sslsplit shell file.

---
+++ mysslsplit.sh
@@ -0,0 +1,12 @@
+mkdir /tmp/sslsplit
+
+sysctl -w net.ipv4.ip_forward=1
+iptables -t nat -F
+iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8080
+iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to-ports 8443
+iptables -t nat -A PREROUTING -p tcp --dport 587 -j REDIRECT --to-ports 8443
+iptables -t nat -A PREROUTING -p tcp --dport 465 -j REDIRECT --to-ports 8443
+iptables -t nat -A PREROUTING -p tcp --dport 993 -j REDIRECT --to-ports 8443
+iptables -t nat -A PREROUTING -p tcp --dport 5222 -j REDIRECT --to-ports 8080
+
+sslsplit -D -l connections.log -j /tmp/sslsplit/ -S logdir/ -k ca.key -c ca.crt ssl 0.0.0.0 8443 tcp 0.0.0.0 8080
