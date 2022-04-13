a487dbe063fdf3965105b0d7e4153dc3aa9e2c8d
Yojahuang <yojahuang30613@gmail.com>
2022-04-13 11:20:54 +0800

    Remove http port in SSLsplit.

--- MITM/mySSLsplit.sh
+++ MITM/mySSLsplit.sh
@@ -3,7 +3,6 @@ mkdir /tmp/sslsplit
 
 sysctl -w net.ipv4.ip_forward=1
 iptables -t nat -F
-iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8080
 iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to-ports 8443
 
-sslsplit -l connections.log -j /tmp/sslsplit/ -S MITM/logdir/ -k ca.key -c ca.crt ssl 0.0.0.0 8443 tcp 0.0.0.0 8080
+sslsplit -l connections.log -j /tmp/sslsplit/ -S MITM/logdir/ -k ca.key -c ca.crt ssl 0.0.0.0 8443
