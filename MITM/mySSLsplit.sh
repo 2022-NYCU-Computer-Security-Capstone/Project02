56d2038c22684a30a5af0352882720cf91c3af6d
Yojahuang <yojahuang30613@gmail.com>
2022-04-14 22:20:59 +0800

    Fix the path of ca.key and ca.crt.

--- MITM/mySSLsplit.sh
+++ MITM/mySSLsplit.sh
@@ -5,4 +5,4 @@ sysctl -w net.ipv4.ip_forward=1
 iptables -t nat -F
 iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to-ports 8443
 
-sslsplit -l connections.log -j /tmp/sslsplit/ -S MITM/logdir/ -k ca.key -c ca.crt ssl 0.0.0.0 8443
+sslsplit -l connections.log -j /tmp/sslsplit/ -S MITM/logdir/ -k MITM/ca.key -c MITM/ca.crt ssl 0.0.0.0 8443
